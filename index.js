5cfea0a804a1134f0463346b665cae302d21b2d5
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-01-04 02:20:32 +0600

    Bistro boss server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('boss is sitting')
+})
+
+app.listen(port, ()=>{
+    console.log(`Bistro boss in sitting on port ${port}`);
+} )
