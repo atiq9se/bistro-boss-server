7ae236a47a7a207cb08bb6577cb3c2e69e454e09
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-01-12 03:01:24 +0600

    role patch in database

--- index.js
+++ index.js
@@ -34,7 +34,38 @@ async function run() {
     
     app.post('/users', async(req, res)=>{
       const user = req.body;
+      //insert email if user doesnt exists
+      //you can do this many ways (1.email unique, 2. upsert 3. simple checking)
+      const query = {email:user.email}
+      const existingUser = await usersCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'user already exists', insertedId: null})
+      }
       const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id:new ObjectId(id)}
+      const updateDoc = {
+        $set:{
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
       res.send(result)
     })
 
