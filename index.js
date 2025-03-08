e0c10eba527b170f73be8759fdd7ed4cfbaec979
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-03-08 22:11:13 +0600

    menu item update

--- index.js
+++ index.js
@@ -125,6 +125,44 @@ async function run() {
         res.send(result)
     })
 
+    app.post('/menu', async(req, res)=>{
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.get('/menu/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.patch('/menu/:id', async(req, res)=>{
+        const item = req.body;
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updatedDoc = {
+          $set:{
+            name: item.name,
+            category:item.category,
+            recipe: item.recipe,
+            price: item.price,
+            image: item.image
+          }
+        }
+        const result = await menuCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+    })
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+
+    })
+
     app.get('/reviews', async(req, res)=>{
         const result = await reviewsCollection.find().toArray();
         res.send(result)
